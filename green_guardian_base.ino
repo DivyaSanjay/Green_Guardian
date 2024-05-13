61cb475901a87e3b19f51a543285450cc9cfcfbb
guillerubio <92153477+guillerubio@users.noreply.github.com>
2024-05-13 11:19:42 -0700

    Now reading lvl

--- green_guardian_base.ino
+++ green_guardian_base.ino
@@ -2,22 +2,24 @@
 #include <DHT.h>
 #include <Wire.h>
 #include <BH1750_WE.h>
-
+// Pins
 #define BH1750_ADDRESS 0x23
 #define DHTPIN A2
 #define DHTTYPE DHT11
+#define WATER_LEVEL_PIN 7
 // Valve constants
 #define VALVE_OPEN_DEGREES 90
 #define VALVE_CLOSED_DEGREES 0
-
+// Sensor objects
 BH1750_WE myBH1750(BH1750_ADDRESS);
 DHT dht(DHTPIN, DHTTYPE);
 Servo myServo;
-
+// Global state variables
 bool valveOpen = false; 
 
 void setup() {
   Serial.begin(9600);
+  
   myServo.attach(3);
   dht.begin();
   Wire.begin();
@@ -25,6 +27,7 @@ void setup() {
     Serial.println("BH1750 Connection Failed!");
     while (1) {}
   }
+  pinMode(WATER_LEVEL_PIN,INPUT);
   closeValve(); // Ensure the valve is closed on startup
 }
 
@@ -49,12 +52,13 @@ void loop() {
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
   float lightIntensity = myBH1750.getLux();
+  int waterOnLevel=digitalRead(WATER_LEVEL_PIN);
 
-  char dataString[150]; // Buffer for the data string
-  sprintf(dataString, "%lu ms, Temp: %.2f Â°C, Hum: %.2f %%, Light: %.2f Lux",
-          currentTime, temperature, humidity, lightIntensity);
+  char dataString[200]; // Buffer for the data string
+  sprintf(dataString, "%lu ms, Temp: %.2f °C, Hum: %.2f %%, Light: %.2f Lux, Lvl: %.2i ", 
+          currentTime, temperature, humidity, lightIntensity, waterOnLevel);
 
-  if (isnan(temperature) || isnan(humidity) || isnan(lightIntensity)) {
+  if (isnan(temperature) || isnan(humidity) || isnan(lightIntensity) || isnan(waterOnLevel)) {
     Serial.println("Failed to read sensors!");
   } else {
     Serial.println(dataString);
