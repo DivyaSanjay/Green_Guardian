8f3f04566639972260a88394441601761fbfdba8
guillerubio <92153477+guillerubio@users.noreply.github.com>
2024-05-12 20:55:30 -0700

    Base added, reads from all sensors functional
    Missing water level and eneverything solar

---
+++ green_guardian_base.ino
@@ -0,0 +1,71 @@
+#include <Servo.h>
+#include <DHT.h>
+#include <Wire.h>
+#include <BH1750_WE.h>
+
+#define BH1750_ADDRESS 0x23
+#define DHTPIN A2
+#define DHTTYPE DHT11
+// Valve constants
+#define VALVE_OPEN_DEGREES 90
+#define VALVE_CLOSED_DEGREES 0
+
+BH1750_WE myBH1750(BH1750_ADDRESS);
+DHT dht(DHTPIN, DHTTYPE);
+Servo myServo;
+
+bool valveOpen = false; 
+
+void setup() {
+  Serial.begin(9600);
+  myServo.attach(3);
+  dht.begin();
+  Wire.begin();
+  if (!myBH1750.init()) {
+    Serial.println("BH1750 Connection Failed!");
+    while (1) {}
+  }
+  closeValve(); // Ensure the valve is closed on startup
+}
+
+void openValve() {
+  myServo.write(VALVE_OPEN_DEGREES);
+  valveOpen = true;
+  Serial.println("Valve opened.");
+}
+
+void closeValve() {
+  myServo.write(VALVE_CLOSED_DEGREES);
+  valveOpen = false;
+  Serial.println("Valve closed.");
+}
+
+bool isValveOpen() {
+  return valveOpen;
+}
+
+void loop() {
+  unsigned long currentTime = millis();
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  float lightIntensity = myBH1750.getLux();
+
+  char dataString[150]; // Buffer for the data string
+  sprintf(dataString, "%lu ms, Temp: %.2f Â°C, Hum: %.2f %%, Light: %.2f Lux",
+          currentTime, temperature, humidity, lightIntensity);
+
+  if (isnan(temperature) || isnan(humidity) || isnan(lightIntensity)) {
+    Serial.println("Failed to read sensors!");
+  } else {
+    Serial.println(dataString);
+  }
+
+  // Example of manual control, replace with actual conditions
+  if (temperature > 25) {
+    if (!isValveOpen()) openValve();
+  } else {
+    if (isValveOpen()) closeValve();
+  }
+
+  delay(1500);
+}
